5cfee50e5920f51d45e1300c1d562597bdd368ef
t.ashula <dev@ashula.info>
2010-08-09 21:20:45 +0900

    unite form branch

--- src/Dimbula/u1/public_html/link.js
+++ src/Dimbula/u1/public_html/link.js
@@ -61,6 +61,11 @@ if ( !HTMLElement.prototype.firstOrDefault ){
   };
 }
 
+function errmsg(){
+  var args = Array.prototype.slice.call( arguments, 0 ).join( ',' );
+  console.log( args );
+}
+
 function currentServicePath(){
   return _L.pathname.split( '/' )[ 1 ];
 }
@@ -72,11 +77,63 @@ if ( ! dimbula ){
 if ( !dimbula.ui ) {
   dimbula.ui = {};
 }
+function findParentForm( ele ){
+  for ( ;ele.parentNode && ele.getAttribute( 'class' ) !== 'wform'; ele = ele.prantNode );
+  return ele.parentNode ? ele : null;
+}
 (function(){
-  var _F = function(){};
-  _F.prototype._title = "";
+  var _F = function( id, title ){
+    this.id_ = 'F_' + id ;
+    this.title_ = title;
+    this.div_ = null;
+    this.init();
+  };
   _F.prototype.title = function(t){ return ( t ) ? ( this._title = t ) : this._title; };
-  dimbula.ui.forms = _F;
+  _F.prototype.init = function(){
+    function minimize( ele ){
+      ele.setAttribute(
+        'class',
+        ele.getAttribute( 'class' ).replace( /maximized/, 'minimized' ) ) ;
+    }
+    function maximize( ele ){
+      ele.setAttribute(
+        'class',
+        ele.getAttribute( 'class' ).replace( /minimized/, 'maximized' ) ) ;
+    }
+    if ( $( this.id_ ) ) {
+      return;
+    }
+    var f = N( 'div', { 'class' : 'wform' ,'id' : this.id_ } );
+    var cbox = N( 'div', { 'class' : 'controlbox' } );
+    [{ c : 'minimize', t : '_'}, { c : 'maximize', t : '[]'}, { c : 'close', t : 'X' } ].forEach(
+      function( o ){
+        var cb = N( 'span', { 'class' : o.c }, T( o.t ) );
+        cb.addEventListener(
+          'click',
+          function( e ){
+            var t = e.target, pp = findParentForm( t );
+            if ( pp ){
+              switch ( t.getAttribute( 'class' ) ){
+               case 'minimize': minimize( pp.querySelector('.mainpanel') ); break;
+               case 'maximize': maximize( pp.querySelector('.mainpanel') ); break;
+               case 'close' : pp.style.height = 0; break;
+              }
+            }
+          },
+          false );
+        cbox.appendChild( cb );
+      }
+    );
+    f.appendChild( N( 'h2', { 'class' : 'titlebar' }, T( this.title_ ) ) );
+    f.appendChild( cbox );
+    f.appendChild( N( 'div', { 'class' : 'mainpanel' } ) );
+    this.div_ = f;
+  };
+  _F.prototype.addMainContent = function( ele ) {
+    this.div_.querySelector( '.mainpanel' ).appendChild( ele );
+  };
+  
+  dimbula.ui.form = _F;
 }(_W));
 
 var ListView = function(){};
@@ -91,82 +148,41 @@ var IDataView = function( o ) {
     this.gridViewId_ = this.id_ + '_grid'; 
     this.rawViewId_ = this.id_ + '_raw';
     this.pullId_ = this.id_ + '_pull';
-    this.heads_ = this.cols_.map( function(c){ return c.h; } );
-    this.keys_ = this.cols_.map( function(c){ return { 'key' : c.k, 'toStr' : c.toStr }; } );
+    this.heads_ = this.cols_.map( function( c ){ return c.h; } );
+    this.keys_ = this.cols_.map( function( c ){ return { 'key' : c.k, 'toStr' : c.toStr }; } );
     var self = this;
-    var div = $( this.id_ );
-    if ( !div ) {
-      div = N( 'div', { 'id' : this.id_ } );      
-      document.body.appendChild( div );
+    var div = $( self.id_ );
+    if ( !div ) {      
+    } else {
+      div.parentNode.removeChild( div );
     }
-    while ( div.firstChild ) {
-      div.removeChild( div.firstChild );
-    }
-    // create control box
-    div.appendChild( N('h2', {'class':'titlebar'}, self.title_ ) );
-    function minimize(ele){
-      ele.setAttribute(
-        'class',
-        ele.getAttribute('class').replace( /maximized/, 'minimized' ) ) ;
-    }
-    function maximize(ele){
-      ele.setAttribute(
-        'class',
-        ele.getAttribute('class').replace( /minimized/, 'maximized' ) ) ;
-    }
-    (function(){
-      var ce = function(e){
-        var t = e.target, pp = t.parentNode.parentNode;
-        switch ( t.getAttribute('class') ){
-          case 'minimize': minimize(pp.querySelector('.detail')); minimize(pp.querySelector('.raw')); break;
-          case 'maximize': maximize(pp.querySelector('.detail')); maximize(pp.querySelector('.raw')); break;
-          case 'close' : pp.style.height = 0; break;
-        }
-      };
-      
-      var cbox = N('div', { 'class' : 'controlbox' } );
-      [{ c : 'minimize', t : '_'},{ c : 'maximize', t : '[]'},{ c : 'close', t : 'X' } ].forEach(
-        function(o){
-          var cb = N( 'span', { 'class' : o.c }, T( o.t ) );
-          cb.addEventListener('click', ce, false );
-          cbox.appendChild( cb );
-        }
-      );
-      div.appendChild( cbox );
-    }());
+    div = N( 'div', { 'id' : self.id_ } );    
     // create grid view
-    var data = JSON.parse( raw )[ this.key_ ];    
-    var grid = N( 'table', { 'id': this.gridViewId_, 'class':'detail data maximized' } );
-    var heads = this.heads_;
-    var keys = this.keys_;
-    heads.forEach(
-      function ( ele ) {
-        grid.firstOrDefault( 'thead' ).firstOrDefault( 'tr' ).appendChild( N( 'th', {}, T( ele ) ) );
-      }
-    );
+    var grid = N( 'table', { 'id' : self.gridViewId_, 'class' : 'detail data maximized' } );
+    var heads = self.heads_;
+    var keys = self.keys_;
+    heads.forEach( function ( ele ) {
+      grid.firstOrDefault( 'thead' ).firstOrDefault( 'tr' ).appendChild( N( 'th', {}, T( ele ) ) );
+    } );
     try {
-      data.forEach(
-        function( ele ) {
-          var row = N( 'tr' );
-          keys.forEach( 
-            function( k ){ 
-              var v = k.toStr ? k.toStr( ele[ k.key ] ) : ele[ k.key ];
-              row.appendChild( N( 'td', {}, T( v ) ) );
-            } 
-          );
-          grid.firstOrDefault( 'tbody' ).appendChild( row );
-        }   
-      );
+      var data = JSON.parse( raw )[ self.key_ ];    
+      data.forEach( function( ele ) {
+        var row = N( 'tr' );
+        keys.forEach( function( k ){ 
+          row.appendChild( N( 'td', {}, T( k.toStr ? k.toStr( ele[ k.key ] ) : ele[ k.key ] ) ) );
+        } );
+        grid.firstOrDefault( 'tbody' ).appendChild( row );
+      } );
     } catch ( x ) {
-      alert( this.title_ + ' json parse error.' + x + '\n' + raw );
+      errmsg( 'WRANING', this.title_ + ' json parse error.' + x + '\n' + raw );
     }
     div.appendChild( grid );
     // create raw view
-    div.appendChild( N( 'textarea', { 'id' : this.rawViewId_, 'class' : 'raw data maximized' }, T( JSON.stringify( data ) ) ) );
+    div.appendChild( N( 'textarea', { 'id' : self.rawViewId_, 'class' : 'raw data maximized' }, T( JSON.stringify( data ) ) ) );
     // create pull button
-    var btn = N( 'button', { 'id': this.pullId_ }, T( 'Pull ' + this.title_ ) );
-    var key = this.key_;
-    btn.addEventListener( 'click', function(e){ pullLinkData( key ); }, false );
+    var btn = N( 'button', { 'id': self.pullId_ }, T( 'Pull ' + self.title_ ) );
+    var key = self.key_;
+    btn.addEventListener( 'click', function( e ){ pullLinkData( key ); }, false );
     div.appendChild( btn );
     return div;
   };
@@ -255,11 +271,22 @@ var createViewHandlers = {
     ]
   })
 };
+
 /**/
 function createOperaLinkView( raw ){
-  for( var key in createViewHandlers ) {
-    if ( createViewHandlers.hasOwnProperty( key ) ){
-      createViewHandlers[ key ]( raw );
+  for( var type in createViewHandlers ) {
+    if ( createViewHandlers.hasOwnProperty( type ) ){
+      var h = createViewHandlers[ type ];
+      var hbox = $( h.id );
+      if ( !hbox ){
+        var form = new dimbula.ui.form( h.id, h.title );
+        _D.body.appendChild( form.div_ );
+      }
+      else {
+        var form = findParentForm( hbox );
+        hbox.parentNode.removeChild( hbox );
+      }
+      form.addMainContent( h( raw ) );
     }
   }
 }
@@ -269,7 +296,8 @@ function pullLinkData( type ){
   var handler = null;
   if ( !type || type === '' ) {
     handler = createOperaLinkView;
-  } else {
+  }
+  else {
     url += type + '/';
     handler = createViewHandlers[ type ];
   }
@@ -285,6 +313,10 @@ function pullLinkData( type ){
     }
   };
   xhr.send( null );  
+}
+
+function createDataForms(){
+  createOperaLinkView();
 }
 
 function createConfigForm(){
@@ -304,30 +336,45 @@ function createConfigForm(){
       N( 'dd',{}, N( 'input', { 'type' : 'password', 'id' : 'password' } ) ) ) );
   div.appendChild(
     N( 'input', { 'type' : 'hidden', 'id' : 'syncstate' } ) );
-  
-  var btn = N( 'button', { 'id' : 'conf', 'value' : 'config' }, T( 'config' ) ) ;
-  btn.addEventListener('click', function(e){ setConfigs(); }, false );
-  div.appendChild( btn );
   div.querySelector('#username').value = acc.username;
   div.querySelector('#password').value = acc.password;
+  div.querySelector('#syncstate').value = acc.syncstate;
+  
+  var btn = N( 'button', { 'id' : 'conf', 'value' : 'config' }, T( 'config' ) ) ;
+  btn.addEventListener(
+    'click',
+    function( e ){
+      var xhr = new XMLHttpRequest();
+      var u = $( 'username' ).value, p = $( 'password' ).value, s = $( 'syncstate' ).value || 0;
+      var params = 'username=' + u + '&password=' + p ;
+      xhr.open( 'POST', 'conf' );
+      xhr.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded' );
+      //  xhr.setRequestHeader( 'Content-Length', params.length );
+      xhr.onreadystatechange = function(ev) {
+        console.log( xhr );
+        if ( xhr.readyState === 4 ) {
+          if ( xhr.status === 200 ) {
+            acc = { username : u, password : p, syncstate : s };        
+            createConfigForm();
+          }
+        }
+      };
+      xhr.send( params );
+    }, false );
+  div.appendChild( btn );
   return div;
 }
 
-function setConfigs(){
-  var xhr = new XMLHttpRequest();
-  var u = $( 'username' ).value, p = $( 'password' ).value;
-  var params = 'username=' + u + '&password=' + p ;
-  xhr.open( 'POST', 'conf' );
-  xhr.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded' );
-  //  xhr.setRequestHeader( 'Content-Length', params.length );
-  xhr.onreadystatechange = function(ev) {
-    console.log( xhr );
-    if ( xhr.readyState === 4 ) {
-      if ( xhr.status === 200 ) {
-        acc = { username : u, password : p };        
-        createConfigForm();
-      }
-    }
-  };
-  xhr.send( params );
+function createDebugForm(){
+  return ;
 }
+
+_W.addEventListener(
+  'DOMContentLoaded',
+  function(e){
+    createConfigForm();
+    createDataForms();
+    createDebugForm();
+  },
+  false
+);

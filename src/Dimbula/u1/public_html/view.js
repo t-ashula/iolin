d875a9a0d8b9448279905df29f8bf0a5ea21728e
t.ashula <dev@ashula.info>
2010-08-20 19:53:16 +0900

    add:urlfilter rich view

--- src/Dimbula/u1/public_html/view.js
+++ src/Dimbula/u1/public_html/view.js
@@ -28,6 +28,9 @@ IDataView.prototype.richViewId = function(){ return this.id + '_rich'; };
 IDataView.prototype.createView = function( raw ) {
   var self = this;
   var frm = new IOLIN.forms( self.id, self.title );
+  if ( !raw ){
+    return frm;
+  }
   frm.clean();
   try{
     var data = JSON.parse( raw )[ self.key ];      
@@ -59,11 +62,58 @@ IDataView.prototype.createGridView = function( data ){
 IDataView.prototype.createRawView = function( data ) {
   return N( 'div', { 'id' : this.rawViewId(), 'class' : 'raw data maximized' }, N( 'textarea', {}, T( JSON.stringify( data ) ) ) );
 };
+IDataView.prototype.createRichViewBox = function ( data ){
+  return N( 'div', { 'id' : this.richViewId(), 'class' : 'rich data maximized' }, N( 'h2', {}, T( this.key ) ) );  
+};
 IDataView.prototype.createRichView = function( data ) {
-  return N( 'div', { 'id' : this.richViewId(), 'class' : 'rich data maximized' }, N( 'h2', {}, T( this.key ) ) );
+  return this.createRichViewBox( data );
 };
 
 var dataViewers = {};
+dataViewers[ 'urlfilter' ] = new IDataView({
+  'id'     : 'uf'
+  ,'title' : 'Content Block Rule'
+  ,'key'   : 'urlfilter'
+  ,'cols'  : [
+    {  'h' : 'Status',  'k' : 'data_status', 'toStr' : null }
+    ,{ 'h' : 'Type',    'k' : 'type',        'toStr' : null }
+    ,{ 'h' : 'Content', 'k' : 'content',     'toStr' : null }
+    ,{ 'h' : 'UUID',    'k' : 'id',          'toStr' : null }
+  ]
+  ,'createRichView' : function( data ) {
+    var self = this;
+    var exlist = N( 'ul', { 'class' : 'ufexclude' } );
+    var inlist = N( 'ul', { 'class' : 'ufinclude' } );
+    var delev = function( e ){
+      for ( var s = e.target; !( /ufdetail/.test( s.getAttribute( 'class' ) ) ); s = s.parentNode );
+      var p = s.querySelector( '.delkey' );
+      deleteLinkData( { 'urlfilter' : [ p.innerText ] } );
+    };
+    data.forEach(
+      function( d ) {
+        if ( d.type ) {
+          var delbtn = N( 'span', { 'class' : 'del' }, T( 'X' ) );
+          delbtn.addEventListener( 'click', delev, false );
+          var li = N( 'li', { 'class' : 'ufdetail' },
+                      N( 'div', { 'class' : 'cb' },  delbtn ),
+                      N( 'p', {},
+                         N( 'span', { 'class':'delkey' }, T( d.id ) ),
+                         N( 'span', {}, T( d.content ) ) ) ) ;
+          if ( d.type === 'exclude' ) {
+            exlist.appendChild( li );           
+          }
+          else if( d.type === 'include' ){
+            inlist.appendChild( li );
+          }
+        }
+      });
+    var rich = self.createRichViewBox( data );
+    rich.appendChild( exlist );
+    rich.appendChild( inlist );
+    return rich;
+  }
+});
+
 dataViewers[ 'typed_history' ] = new IDataView({
   'id'    : 'th'
   ,'title' : 'Typed History'
@@ -74,25 +124,23 @@ dataViewers[ 'typed_history' ] = new IDataView({
     ,{ 'h' : 'Type',    'k' : 'type',        'toStr' : null }
     ,{ 'h' : 'Content', 'k' : 'content',     'toStr' : null }
   ]
-  , 'createRichView' : function( data ) {
+  ,'createRichView' : function( data ) {
     var self = this;
-    var rich = N( 'div', { 'id' : this.richViewId(), 'class' : 'rich data maximized' }, N( 'h3', {}, T( self.title  ) ) );
+    var rich = self.createRichViewBox( data );
+    //N( 'div', { 'id' : this.richViewId(), 'class' : 'rich data maximized' }, N( 'h3', {}, T( self.title  ) ) );
     var createTHBox = function( d ){
       var delbtn = N( 'span', { 'class' : 'del' }, T( 'X' ) );
-      delbtn.addEventListener( 'click', function(e){
-        var s = e.target;
-        while ( !(/thdetail/.test( s.getAttribute( 'class' ) ) ) ){
-          s = s.parentNode;
-        }
-        var p = s.querySelector( '.thcontent' );
-        deleteLinkData( { 'typed_history' : p.innerText } );
+      delbtn.addEventListener( 'click', function( e ){
+        for ( var s = e.target; !( /thdetail/.test( s.getAttribute( 'class' ) ) ); s = s.parentNode );
+        var p = s.querySelector( '.delkey' );
+        deleteLinkData( { 'typed_history' : [ p.innerText ] } );
       }, false );
-      var box = N( 'div', { 'class' : 'thdetail ' + d[ 'type' ] },
-                   N( 'p', {'class' : 'thcontent' }, T( d[ 'content' ] ) ),                   
-                   N( 'span', {}, T( d[ 'last_typed' ] ) ),
-                   N( 'div', { 'class' : 'cb' },  delbtn )
-                 );
-      return box;
+      return N( 'div', { 'class' : 'thdetail' },
+                N( 'div', { 'class' : 'cb' },  delbtn ),
+                N( 'p', {'class' : 'thcontent' + ' ' + d[ 'type' ] },
+                   N( 'span', { 'class' : 'datetime' }, T( d[ 'last_typed' ] ) ),
+                   N( 'span', { 'class' : 'delkey content' }, T( d[ 'content' ] ) )
+                 ) );
     };
     data.forEach( function( d ){
       rich.appendChild( createTHBox( d ) );
@@ -101,7 +149,7 @@ dataViewers[ 'typed_history' ] = new IDataView({
   }
 });
 
-dataViewers['speeddial'] = new IDataView( {
+dataViewers[ 'speeddial' ] = new IDataView( {
   'id'    : 'sd'
   ,'title' : 'Speed Dial'
   ,'key'   : 'speeddial'
@@ -115,12 +163,12 @@ dataViewers['speeddial'] = new IDataView( {
     ,{ 'h' : 'Interval',    'k' : 'interval',     'toStr' : null }
   ]
 });
-dataViewers['search_engine'] = new IDataView({
+dataViewers[ 'search_engine' ] = new IDataView({
   'id'     : 'se'
   ,'title' : 'Search Engine'
   ,'key'   : 'search_engine'
   ,'cols'  : [
-    { 'h' : 'Status',               'k' : 'data_status', 'toStr' : null }
+    {  'h' : 'Status',               'k' : 'data_status', 'toStr' : null }
     ,{ 'h' : 'ID',                   'k' : 'uuid',        'toStr' : null  }
     ,{ 'h' : 'Type',                 'k' : 'type',        'toStr' : null  }
     ,{ 'h' : 'Group',                'k' : 'group',       'toStr' : null  }
@@ -136,12 +184,12 @@ dataViewers['search_engine'] = new IDataView({
     ,{ 'h' : 'ICON',                 'k' : 'icon',    'toStr' : null  }
   ]
 });
-dataViewers['note'] = new IDataView( {
+dataViewers[ 'note' ] = new IDataView( {
   'id'    : 'nt'
   ,'title' : 'Notes'
   ,'key'   : 'note'
   ,'cols'  : [
-    { 'h' : 'Status',  'k' : 'data_status', 'toStr' : null }
+    {  'h' : 'Status',  'k' : 'data_status', 'toStr' : null }
     ,{ 'h' : 'Type',    'k' : 'type',        'toStr' : null }
     ,{ 'h' : 'Content', 'k' : 'content',     'toStr' : null }
     ,{ 'h' : 'Title',   'k' : 'title',       'toStr' : null }
@@ -152,12 +200,12 @@ dataViewers['note'] = new IDataView( {
     ,{ 'h' : 'Prev',    'k' : 'previous',    'toStr' : null }
   ]
 });
-dataViewers['bookmark'] = new IDataView( {
+dataViewers[ 'bookmark' ] = new IDataView( {
   'id'    : 'bm'
   ,'title' : 'Bookmarks'
   ,'key'   : 'bookmark'
   ,'cols'  : [
-    { 'h' : 'Status',  'k' : 'data_status', 'toStr' : null }
+    {  'h' : 'Status',  'k' : 'data_status', 'toStr' : null }
     ,{ 'h' : 'Type',    'k' : 'type',        'toStr' : null }
     ,{ 'h' : 'URI',     'k' : 'uri',         'toStr' : null }
     ,{ 'h' : 'Title',   'k' : 'title',       'toStr' : null }
@@ -172,14 +220,4 @@ dataViewers['bookmark'] = new IDataView( {
   ]
 });
 
-dataViewers['urlfilter' ] = new IDataView({
-  'id'    : 'uf'
-  ,'title' : 'Content Block Rule'
-  ,'key'   : 'urlfilter'
-  ,'cols'  : [
-    { 'h' : 'Status',  'k' : 'data_status', 'toStr' : null }
-    ,{ 'h' : 'Type',    'k' : 'type',        'toStr' : null }
-    ,{ 'h' : 'Content', 'k' : 'content',     'toStr' : null }
-  ]
-});
 
